3c11a626df34dbc78f9af4c5e1a06c5e3fd8f686
21900646 <69943167+21900646@users.noreply.github.com>
2022-01-26 10:04:48 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,7 @@
+################################################################ gstreamer.py
+################################################################ gstreamer.py
+################################################################ gstreamer.py
+
 import sys 
 import threading
 
@@ -281,3 +285,227 @@ def run_pipeline(user_function,
 
     pipeline = GstPipeline(pipeline, user_function, src_size)
     pipeline.run()
+    
+    
+    
+    
+################################################################ classify.py
+################################################################ classify.py
+################################################################ classify.py
+
+import argparse
+import gstreamer
+import os
+import time
+
+from common import avg_fps_counter, SVG
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
+from pycoral.adapters.common import input_size
+from pycoral.adapters.classify import get_classes
+
+def generate_svg(size, text_lines):
+    svg = SVG(size)
+    for y, line in enumerate(text_lines, start=1):
+      svg.add_text(10, y * 20, line, 20)
+    return svg.finish()
+
+def main():
+    default_model_dir = '../all_models'
+    default_model = 'mobilenet_v2_1.0_224_quant_edgetpu.tflite'
+    default_labels = 'imagenet_labels.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--threshold', type=float, default=0.1,
+                        help='classifier score threshold')
+    parser.add_argument('--videosrc', help='Which video source to use. ',
+                        default='/dev/video0')
+    parser.add_argument('--headless', help='Run without displaying the video.',
+                        default=False, type=bool)
+    parser.add_argument('--videofmt', help='Input video format.',
+                        default='raw',
+                        choices=['raw', 'h264', 'jpeg'])
+    args = parser.parse_args()
+
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    labels = read_label_file(args.labels)
+    inference_size = input_size(interpreter)
+
+    # Average fps over last 30 frames.
+    fps_counter = avg_fps_counter(30)
+
+    def user_callback(input_tensor, src_size, inference_box):
+      nonlocal fps_counter
+      start_time = time.monotonic()
+      run_inference(interpreter, input_tensor)
+
+      results = get_classes(interpreter, args.top_k, args.threshold)
+      end_time = time.monotonic()
+      text_lines = [
+          ' ',
+          'Inference: {:.2f} ms'.format((end_time - start_time) * 1000),
+          'FPS: {} fps'.format(round(next(fps_counter))),
+      ]
+      for result in results:
+          text_lines.append('score={:.2f}: {}'.format(result.score, labels.get(result.id, result.id)))
+      print(' '.join(text_lines))
+      return generate_svg(src_size, text_lines)
+
+    result = gstreamer.run_pipeline(user_callback,
+                                    src_size=(640, 480),
+                                    appsink_size=inference_size,
+                                    videosrc=args.videosrc,
+                                    videofmt=args.videofmt,
+                                    headless=args.headless)
+
+if __name__ == '__main__':
+    main()
+
+    
+################################################################ common.py
+################################################################ common.py
+################################################################ common.py
+
+
+import collections
+import io
+import time
+
+SVG_HEADER = '<svg width="{w}" height="{h}" version="1.1" >'
+SVG_RECT = '<rect x="{x}" y="{y}" width="{w}" height="{h}" stroke="{s}" stroke-width="{sw}" fill="none" />'
+SVG_TEXT = '''
+<text x="{x}" y="{y}" font-size="{fs}" dx="0.05em" dy="0.05em" fill="black">{t}</text>
+<text x="{x}" y="{y}" font-size="{fs}" fill="white">{t}</text>
+'''
+SVG_FOOTER = '</svg>'
+
+def avg_fps_counter(window_size):
+    window = collections.deque(maxlen=window_size)
+    prev = time.monotonic()
+    yield 0.0  # First fps value.
+
+    while True:
+        curr = time.monotonic()
+        window.append(curr - prev)
+        prev = curr
+        yield len(window) / sum(window)
+
+class SVG:
+    def __init__(self, size):
+        self.io = io.StringIO()
+        self.io.write(SVG_HEADER.format(w=size[0] , h=size[1]))
+
+    def add_rect(self, x, y, w, h, stroke, stroke_width):
+        self.io.write(SVG_RECT.format(x=x, y=y, w=w, h=h, s=stroke, sw=stroke_width))
+
+    def add_text(self, x, y, text, font_size):
+        self.io.write(SVG_TEXT.format(x=x, y=y, t=text, fs=font_size))
+
+    def finish(self):
+        self.io.write(SVG_FOOTER)
+        return self.io.getvalue()
+
+
+    
+    
+################################################################ detect.py
+################################################################ detect.py
+################################################################ detect.py
+
+
+import argparse
+import gstreamer
+import os
+import time
+
+from common import avg_fps_counter, SVG
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
+
+def generate_svg(src_size, inference_box, objs, labels, text_lines):
+    svg = SVG(src_size)
+    src_w, src_h = src_size
+    box_x, box_y, box_w, box_h = inference_box
+    scale_x, scale_y = src_w / box_w, src_h / box_h
+
+    for y, line in enumerate(text_lines, start=1):
+        svg.add_text(10, y * 20, line, 20)
+    for obj in objs:
+        bbox = obj.bbox
+        if not bbox.valid:
+            continue
+        # Absolute coordinates, input tensor space.
+        x, y = bbox.xmin, bbox.ymin
+        w, h = bbox.width, bbox.height
+        # Subtract boxing offset.
+        x, y = x - box_x, y - box_y
+        # Scale to source coordinate space.
+        x, y, w, h = x * scale_x, y * scale_y, w * scale_x, h * scale_y
+        percent = int(100 * obj.score)
+        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+        svg.add_text(x, y - 5, label, 20)
+        svg.add_rect(x, y, w, h, 'red', 2)
+    return svg.finish()
+
+def main():
+    default_model_dir = '../all_models'
+    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+    default_labels = 'coco_labels.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--threshold', type=float, default=0.1,
+                        help='classifier score threshold')
+    parser.add_argument('--videosrc', help='Which video source to use. ',
+                        default='/dev/video0')
+    parser.add_argument('--videofmt', help='Input video format.',
+                        default='raw',
+                        choices=['raw', 'h264', 'jpeg'])
+    args = parser.parse_args()
+
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    labels = read_label_file(args.labels)
+    inference_size = input_size(interpreter)
+
+    # Average fps over last 30 frames.
+    fps_counter = avg_fps_counter(30)
+
+    def user_callback(input_tensor, src_size, inference_box):
+      nonlocal fps_counter
+      start_time = time.monotonic()
+      run_inference(interpreter, input_tensor)
+      # For larger input image sizes, use the edgetpu.classification.engine for better performance
+      objs = get_objects(interpreter, args.threshold)[:args.top_k]
+      end_time = time.monotonic()
+      text_lines = [
+          'Inference: {:.2f} ms'.format((end_time - start_time) * 1000),
+          'FPS: {} fps'.format(round(next(fps_counter))),
+      ]
+      print(' '.join(text_lines))
+      return generate_svg(src_size, inference_box, objs, labels, text_lines)
+
+    result = gstreamer.run_pipeline(user_callback,
+                                    src_size=(640, 480),
+                                    appsink_size=inference_size,
+                                    videosrc=args.videosrc,
+                                    videofmt=args.videofmt)
+
+if __name__ == '__main__':
+    main()
